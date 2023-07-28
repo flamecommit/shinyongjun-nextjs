8152634ee9de0c8e5fabb0c41e3b2d456ada8e0b
신용준 <shinyj1991@gmail.com>
2023-07-28 22:16:47 +0900

    modify ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
     {
       name: "shinyongjun.com",
       exec_mode: "cluster",
-      script: "next start",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
       env_prod: {
         HOST: "0.0.0.0",
         PORT: 3000,
