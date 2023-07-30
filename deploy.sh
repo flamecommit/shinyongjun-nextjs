941d84cb736870207f910e59e4234e20f4a0c942
신용준 <shinyj1991@gmail.com>
2023-07-30 11:10:51 +0900

    deploy

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-REPOSITORY=/var/www/html/shinyongjun-nextjs # 배포된 프로젝트 경로.
-
-cd $REPOSITORY
-
-sudo /usr/bin/yarn install
-sudo /usr/bin/yarn build
-sudo /usr/bin/pm2 reload ecosystem.config.js --env=prod
