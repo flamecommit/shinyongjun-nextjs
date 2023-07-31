1e83e81de87708033255b2bbeec92b32abbda042
신용준 <shinyj1991@wemadeazoutlook.onmicrosoft.com>
2023-07-31 14:25:14 +0900

    test

--- src/app/blog/[slugs]/page.tsx
+++ src/app/blog/[slugs]/page.tsx
@@ -1,20 +1,31 @@
 import { getAllPosts } from "@/libs/post";
-import { serializeMdx } from "@/libs/mdx";
 import { MDXRemote } from "next-mdx-remote/rsc";
+
+type PostMatter = {
+  title: string;
+  description: string;
+  tags: string[];
+  draft?: boolean;
+  date: string;
+};
+
+type Post = PostMatter & {
+  slug: string;
+  content: string;
+  wordCount: number;
+};
 
 export default async function PostPage({ params }: { params: object }) {
   const { slugs } = params as { slugs: string[] };
 
-  const post: any = getAllPosts().find((v) => v.slug === `/blog/${slugs}`);
-  const mdx: any = await serializeMdx(post.content);
-
-  console.log("----------------------------------------");
-  console.log(mdx);
+  const post: Post | undefined = getAllPosts().find(
+    (v) => v.slug === `/blog/${slugs}`
+  );
 
   return (
     <div>
-      <div className="title">{post.title}</div>
-      <MDXRemote source={post.content} />
+      <div className="title">{post?.title}</div>
+      {post?.content ? <MDXRemote source={post?.content} /> : ""}
     </div>
   );
 }
