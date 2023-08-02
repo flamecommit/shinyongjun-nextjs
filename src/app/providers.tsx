a1c92e234aabcdb84f188940ca93dcb38fa74e0a
신용준 <shinyj1991@wemadeazoutlook.onmicrosoft.com>
2023-08-02 11:35:24 +0900

    ssr metadata 적용

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,12 +2,13 @@
 
 import { PropsWithChildren } from "react";
 import { ResetStyle } from "@/styles/reset";
+import Providers from "@/stores/provider";
 
-export function Providers({ children }: PropsWithChildren) {
+export function RootProviders({ children }: PropsWithChildren) {
   return (
-    <>
+    <Providers>
       <ResetStyle />
       {children}
-    </>
+    </Providers>
   );
 }
