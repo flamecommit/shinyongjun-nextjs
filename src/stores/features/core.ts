2dc35b198db753943a32a1584a9bd0e14427fe66
신용준 <shinyj1991@wemadeazoutlook.onmicrosoft.com>
2023-08-02 11:36:03 +0900

    setting : redux/toolkit

---
+++ src/stores/features/core.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+type LocaleType = "ko" | "en";
+
+export interface CoreState {
+  locale: "ko" | "en";
+}
+
+const initialState: CoreState = {
+  locale: "ko",
+};
+
+export const coreSlice = createSlice({
+  name: "core",
+  initialState,
+  reducers: {
+    changeLocale: (state, action: PayloadAction<LocaleType>) => {
+      state.locale = action.payload;
+    },
+  },
+});
+
+export const coreActions = coreSlice.actions;
+
+export default coreSlice.reducer;
