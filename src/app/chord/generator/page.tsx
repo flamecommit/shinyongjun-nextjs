de0ffa90895d5c10944982d7964f7fe98945ce44
신용준 <shinyj1991@gmail.com>
2023-08-24 05:48:25 +0900

    feat : chord finder generateMetadata

--- src/app/chord/generator/page.tsx
+++ src/app/chord/generator/page.tsx
@@ -2,15 +2,20 @@ import type { Metadata } from 'next';
 import PageTitle from '@/components/page/Title';
 import ChordFinder from '@/components/chord/Finder';
 
-export const metadata: Metadata = {
-  title: 'Guitar Chord Finder',
-  description: `You can use the fretboard to create the chord.`,
-  openGraph: {
-    title: `Guitar Chord Finder`,
-    description: `You can use the fretboard to create the chord.`,
-    images: process.env.OG_IMAGE,
-  },
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const title = 'Guitar Chord Finder';
+  const description = `You can use the fretboard to create the chord.`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: process.env.OG_IMAGE,
+    },
+  };
+}
 
 const DiaryPage = async () => {
   return (
