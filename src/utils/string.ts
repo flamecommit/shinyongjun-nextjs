1666be2b9ef2ecac9f34d2b2b2654eb7c6641416
신용준 <shinyj1991@wemadeazoutlook.onmicrosoft.com>
2023-08-24 13:50:31 +0900

    feat : 함수 주석 추가

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,2 +1,6 @@
+/**
+ * 첫 글자를 알파벳 대문자로 바꿔주는 함수
+ * ex: hello world => Hello world
+ */
 export const capitalizeString = (input: string): string =>
   input.length ? input[0].toUpperCase() + input.slice(1) : input;
