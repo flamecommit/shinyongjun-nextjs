39f7e77045317a95ecb21bdba71acaca8c1571bb
신용준 <shinyj1991@wemadeazoutlook.onmicrosoft.com>
2023-08-25 15:38:53 +0900

    feat : test component

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+import PageTitle from '@/components/page/Title';
+import SnippetTest from '@/components/snippet/Test';
+
+async function TestPage() {
+  return (
+    <>
+      <PageTitle>Test</PageTitle>
+      <SnippetTest />
+    </>
+  );
+}
+
+export default TestPage;
---
+++ src/components/snippet/Test.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import styled from 'styled-components';
+
+function SnippetTest() {
+  return (
+    <StyledSnippetTest>
+      <div>teset</div>
+    </StyledSnippetTest>
+  );
+}
+
+const StyledSnippetTest = styled.div`
+  padding: 50px;
+`;
+
+export default SnippetTest;
