6d3d77e752c7a7da0f7830825cb79ba32607fdec
신용준 <shinyj1991@gmail.com>
2023-08-26 10:41:10 +0900

    feat : @shinyongjun/react-fullpage 모듈 추가

--- src/components/snippet/Test.tsx
+++ src/components/snippet/Test.tsx
@@ -1,11 +1,29 @@
 'use client';
 
 import styled from 'styled-components';
+import {
+  FullpageContainer,
+  FullpageSection,
+} from '@shinyongjun/react-fullpage';
 
 function SnippetTest() {
   return (
     <StyledSnippetTest>
       <div>teset</div>
+      <FullpageContainer>
+        <FullpageSection>
+          <div>1</div>
+        </FullpageSection>
+        <FullpageSection>
+          <div>1</div>
+        </FullpageSection>
+        <FullpageSection>
+          <div>1</div>
+        </FullpageSection>
+        <FullpageSection>
+          <div>1</div>
+        </FullpageSection>
+      </FullpageContainer>
     </StyledSnippetTest>
   );
 }
