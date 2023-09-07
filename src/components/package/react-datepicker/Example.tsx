aa823ab00c11be07872f4c96d4860ba5f9aac5a8
신용준 <shinyj1991@wemadeazoutlook.onmicrosoft.com>
2023-09-07 11:26:00 +0900

    feat : move datepicker example file

--- src/components/package/react-datepicker/Example.tsx
+++ src/components/package/react-datepicker/Example.tsx
@@ -3,7 +3,7 @@ import ReactDatepickerMdx from './Mdx';
 
 async function PackageReactDatepickerExample() {
   const document = await getSingleDocument(
-    `/contents/example/react-datepicker/index.mdx`,
+    `/src/components/package/react-datepicker/index.mdx`,
   );
 
   return <ReactDatepickerMdx documentData={document} />;
